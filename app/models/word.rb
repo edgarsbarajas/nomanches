247f0cfd3a964a629b5e439e65aae19090ca1698
Edgar Barajas <edgarsbarajas@yahoo.com>
2019-04-19 11:58:49 -0700

    Add comments model and associations for Comment and Word. This makes it possible to comment on comments and words.

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,3 +1,4 @@
 class Word < ApplicationRecord
   belongs_to :user
+  has_many :comments, as: :commentable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_19_040748) do
+ActiveRecord::Schema.define(version: 2019_04_19_180057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "comment"
+    t.integer "commentable_id"
+    t.string "commentable_type"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
