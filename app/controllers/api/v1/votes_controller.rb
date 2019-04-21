fadb3c91d6e74c38c2860043b87ba8155392e158
Edgar Barajas <edgarsbarajas@yahoo.com>
2019-04-21 13:53:19 -0700

    User can update their upvote true to an upvote false

--- app/controllers/api/v1/votes_controller.rb
+++ app/controllers/api/v1/votes_controller.rb
@@ -12,6 +12,26 @@ class Api::V1::VotesController < Api::V1::ApiController
     end
   end
 
+  def update
+    vote = Vote.find(params[:id])
+
+    p "*" * 100
+    p vote_params
+    if current_user.id == vote.user_id
+      if vote && vote.update_columns(upvote: vote_params[:upvote])
+        render json: vote
+      else
+        render json: {
+          vote: "Error updating vote"
+        }, status: 400
+      end
+    else
+      render json: {
+        vote: "You are not allowed to update this vote"
+      }, status: 401
+    end
+  end
+
   private
   def vote_params
     params.require(:vote).permit(:voteable_id, :voteable_type, :upvote)
