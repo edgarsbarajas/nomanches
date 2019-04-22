f406090622d223b33143877d7de7eeea3f371a39
Edgar Barajas <edgarsbarajas@yahoo.com>
2019-04-22 08:06:31 -0700

    Users are able to sign up

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::UsersController < ApplicationController
     if user.save
       render json: user
     else
-      render json: user.errors.full_messages
+      render json: user.errors, status: 400
     end
   end
 
