a2d04a34c5323212372e0ed672de8bf922e6a639
Edgar Barajas <edgarsbarajas@yahoo.com>
2019-04-23 15:38:37 -0700

    react router has been setup. Still need to add links to the menu modal and nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
+import Feed from './components/Feed';
+import Add from './components/Add';
 import Nav from './components/nav/Nav';
 import './App.css';
 
@@ -8,9 +11,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Router>
         <Nav />
-      </div>
+        <div className='container'>
+          <Switch>
+            <Route path='/' exact component={Feed}/>
+            <Route path='/login' component={Login}/>
+            <Route path='/register' component={Register}/>
+            <Route path='/add' component={Add}/>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
