d81751d353787006452d02bef2fb657079fbfc4c
Edgar Barajas <edgarsbarajas@yahoo.com>
2019-04-27 08:12:54 -0700

    Words are returned whther or not there is a current user. Votes and comments/nested comments are included in the json

--- app/controllers/api/v1/words_controller.rb
+++ app/controllers/api/v1/words_controller.rb
@@ -1,8 +1,32 @@
 class Api::V1::WordsController < Api::V1::ApiController
-  before_action :require_login
+  before_action :require_login, only: [:create]
+
+  def index
+    if !current_user
+      words = Word.all.as_json(include: {
+        comments: {
+          include: :comments
+        },
+        votes: {}
+      })
+
+      render json: words
+    else
+      # include the user id in the votes portion
+      words = Word.all.as_json(include: {
+        comments: {
+          include: :comments
+        },
+        votes: {
+          only: [:user_id, :upvote]
+        }
+      })
+
+      render json: words
+    end
+  end
 
   def create
-    p "*" * 1000
     word = current_user.words.new(word_params)
 
     if word.save
@@ -14,6 +38,6 @@ class Api::V1::WordsController < Api::V1::ApiController
 
   private
   def word_params
-    params.require(:word).permit(:word, :definition, :example)
+    params.require(:word).permit(:word, :definition, :example, :comments_count)
   end
 end
