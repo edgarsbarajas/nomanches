970731af6afa0636a35031b3e5939a5b1a560490
Edgar Barajas <edgarsbarajas@yahoo.com>
2019-09-10 21:32:33 -0700

    Update word all done

--- controllers/votes.js
+++ controllers/votes.js
@@ -34,6 +34,44 @@ router.post('/word/:word_id', authorizeUser, (req, res) => {
 // Read vote
 
 // Update vote
+router.put('/:id', authorizeUser, (req, res) => {
+  Vote.findOne({ _id: req.params.id })
+    .then(vote => {
+      if(!vote) return res.status(404).json({ Vote: 'No vote found.' });
+      // Boot the user if they did not create this vote
+      if(vote.user != req.current_user.id) return res.status(403).json({ Forbidden: 'You do not have access to update this vote.' });
+
+      // Update the Vote document to true or false
+      return Vote.findOneAndUpdate(
+        { _id: vote.id },
+        { '$set': { upvote: !vote.upvote }, '$inc': { __v: 1 } },
+        { runValidators: true, context: 'query' }
+      )
+        .then(unmodifiedVote => {
+          // Now that we have the updated vote,
+          // we have to update the Word doc so the reference is in the right category (up vs down )
+          // Find the word doc first
+          return Word.findOne({ _id: unmodifiedVote.word })
+            .then(word => {
+              if(!word) return res.status(404).json({ Word: 'No word found.' });
+              
+              // Where do we want to pull from and push to? Whatever the unmodified vote upvote vslue is
+              if(unmodifiedVote.upvote) {
+                word.votes.up.pull(unmodifiedVote.id);
+                word.votes.down.push(unmodifiedVote.id);
+              } else {
+                word.votes.down.pull(unmodifiedVote.id);
+                word.votes.up.push(unmodifiedVote.id);
+              }
+
+              // Save the Word doc
+              return word.save()
+                .then(word => res.json(word))
+            })
+        })
+    })
+    .catch(error => res.status(400).json(error));
+});
 
 // Delete vote
 router.delete('/:id', authorizeUser, (req, res) => {
--- helpers/index.js
+++ helpers/index.js
@@ -4,7 +4,7 @@ const generateAuthToken = (req, res, payload) => {
   jwt.sign(
     { payload },
     process.env['JWT_SECRET'],
-    { expiresIn: '1h' },
+    { expiresIn: '48h' },
     (error, token) => {
       if(error) return res.status(400).json(error);
       return res.json({token});
