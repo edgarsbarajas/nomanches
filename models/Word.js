fe78d125f8411d1e34698e4ade805f3ea01c8ef1
Edgar Barajas <edgarsbarajas@yahoo.com>
2019-09-14 23:22:15 -0700

    fix add word CREATE on client to work, and edit error messages

--- models/Word.js
+++ models/Word.js
@@ -3,20 +3,21 @@ const mongoose = require('mongoose');
 const wordSchema = new mongoose.Schema({
   value: {
     type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 120
+    required: [true, 'word is required'],
+    minlength: [2, 'word must be at least 2 characters'],
+    maxlength: [120, 'word can not be over 120 characters']
   },
   definition: {
     type: String,
-    minlength: 6,
-    maxlength: 120
+    required: [true, 'definition is required'],
+    minlength: [6, 'definition must be at least 6 characters'],
+    maxlength: [120, 'definition can not be over 120 characters']
   },
   example: {
     type: String,
-    required: true,
-    minlength: 6,
-    maxlength: 120
+    required: [true, 'example is required'],
+    minlength: [6, 'example must be at least 6 characters'],
+    maxlength: [120, 'example can not be over 120 characters']
   },
   user: {
     type: mongoose.Schema.ObjectId,
