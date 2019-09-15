5d10110d91b1d32d0010bc12938e3411f7830ef0
Edgar Barajas <edgarsbarajas@yahoo.com>
2019-09-15 01:10:35 -0700

    Feed is now infinite scrolling

--- client/src/components/feed/Feed.js
+++ client/src/components/feed/Feed.js
@@ -2,17 +2,30 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
 import Word from './Word';
+import BottomScrollListener from 'react-bottom-scroll-listener';
 
-// COOL TO HAVE: global error box modal
 class Feed extends Component {
   state = {
-    feed: []
+    feed: [],
+    page: 1
   };
+
   componentDidMount() {
+    this.fetchWords();
+  }
+
+  fetchWords() {
     axios
-      .get('/words/feed/1')
-      .then(response => this.setState({ feed: response.data }))
+      .get(`/words/feed/${this.state.page}`)
+      .then(response => this.setState({ feed: [...this.state.feed, ...response.data] }))
       .catch(error => console.log(error))
+  }
+
+  hasReachedBottom = () => {
+    // Increase the page number and fetch the new batch of words
+    this.setState({ page: this.state.page + 1 }, () => {
+      this.fetchWords();
+    })
   }
 
   render() {
@@ -20,6 +33,7 @@ class Feed extends Component {
 
     return (
       <div className='feed'>
+        <BottomScrollListener onBottom={this.hasReachedBottom} />
         { this.state.feed.map(word => <Word key={word._id} word={word} />) }
       </div>
     );
