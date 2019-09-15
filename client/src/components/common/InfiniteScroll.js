5cd0c160595c4d4f8fa980121953db6599d64fda
Edgar Barajas <edgarsbarajas@yahoo.com>
2019-09-15 01:26:28 -0700

    Create resuable infinite-scroll component

---
+++ client/src/components/common/InfiniteScroll.js
@@ -0,0 +1,44 @@
+import React, { Component, Fragment } from 'react';
+import axios from 'axios';
+import { connect } from 'react-redux';
+import Word from '../feed/Word';
+import BottomScrollListener from 'react-bottom-scroll-listener';
+
+class InfiniteScroll extends Component {
+  state = {
+    content: [],
+    page: 1
+  };
+
+  componentDidMount() {
+    console.log('wtf');
+    this.fetchWords();
+  }
+
+  fetchWords() {
+    axios
+      .get(`${this.props.url}/${this.state.page}`)
+      .then(response => this.setState({ content: [...this.state.content, ...response.data] }))
+      .catch(error => console.log(error.response))
+  }
+
+  hasReachedBottom = () => {
+    // Increase the page number and fetch the new batch of words
+    this.setState({ page: this.state.page + 1 }, () => {
+      this.fetchWords();
+    })
+  }
+
+  render() {
+    if(this.state.content.length <= 0) return null;
+
+    return (
+      <div className='infinite-scroll'>
+        <BottomScrollListener onBottom={this.hasReachedBottom} />
+        { this.state.content.map(word => <Word key={word._id} word={word} />) }
+      </div>
+    );
+  }
+}
+
+export default InfiniteScroll;
--- client/src/components/feed/Feed.js
+++ client/src/components/feed/Feed.js
@@ -2,39 +2,15 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
 import Word from './Word';
-import BottomScrollListener from 'react-bottom-scroll-listener';
+import InfiniteScroll from '../common/InfiniteScroll';
 
 class Feed extends Component {
-  state = {
-    feed: [],
-    page: 1
-  };
-
-  componentDidMount() {
-    this.fetchWords();
-  }
-
-  fetchWords() {
-    axios
-      .get(`/words/feed/${this.state.page}`)
-      .then(response => this.setState({ feed: [...this.state.feed, ...response.data] }))
-      .catch(error => console.log(error))
-  }
-
-  hasReachedBottom = () => {
-    // Increase the page number and fetch the new batch of words
-    this.setState({ page: this.state.page + 1 }, () => {
-      this.fetchWords();
-    })
-  }
-
   render() {
-    if(this.state.feed.length <= 0) return null;
-
     return (
       <div className='feed'>
-        <BottomScrollListener onBottom={this.hasReachedBottom} />
-        { this.state.feed.map(word => <Word key={word._id} word={word} />) }
+        <InfiniteScroll
+          url='/words/feed'
+        />
       </div>
     );
   }
