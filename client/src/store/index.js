1cae92d432ec3692eba5e93d2c403b032f9b65d8
Edgar Barajas <edgarsbarajas@yahoo.com>
2019-10-05 16:12:03 -0700

    redux persist has been ripped out

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,21 +1,6 @@
 import { createStore, applyMiddleware, } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import ReduxThunk from 'redux-thunk';
 import rootReducer from '../reducers';
 import { setAuthorizationHeader } from '../helpers';
 
-
-const persistConfig = {
- key: 'root',
- storage: storage,
- stateReconciler: autoMergeLevel2,
- blacklist: ['globalModalComponent']
-};
-
-export const store = createStore(persistReducer(persistConfig, rootReducer), applyMiddleware(ReduxThunk));
-export const persistor = persistStore(store, {}, () => {
-    const currentUser = store.getState().auth.user;
-    setAuthorizationHeader(currentUser.token);
-  });
+export default createStore(rootReducer, applyMiddleware(ReduxThunk));
