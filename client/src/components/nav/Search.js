7cbef210f7df3c039eff508dff795f349de955b7
Edgar Barajas <edgarsbarajas@yahoo.com>
2019-11-06 20:54:23 -0800

    Search results dropdown styling is done for now. I would still like to make the text bold where the search value exists in the shirt result

--- client/src/components/nav/Search.js
+++ client/src/components/nav/Search.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import classnames from 'classnames';
 import SVG from '../common/SVG';
 
 class Search extends Component {
   state = {
-    searchValue: ''
+    searchValue: '',
+    searchResults: ['tacones', 'tacefdde', 'tqefef', 'teranos']
   };
 
   handleValueChange = event => {
@@ -17,18 +20,37 @@ class Search extends Component {
   }
 
   render() {
+    const hasValidSearchResults = this.state.searchResults.length > 0 && this.state.searchValue;
+
     return (
-      <form className='search mr-l full' onSubmit={this.handleSearchSubmit}>
-        <input
-          type='text'
-          value={this.state.value}
-          onChange={this.handleValueChange}
-          className='br fs-r pr-m pl-m pt-s pb-s full'
-        />
-      <button className='bg-dark fc-light'>
-        <SVG fill='#fff' width={20} viewBox='0 0 179.76 179.92' name='search' />
-      </button>
-    </form>
+      <div className='search p-r mr-l full'>
+        <form className='full' onSubmit={this.handleSearchSubmit}>
+          <input
+            type='text'
+            value={this.state.value}
+            onChange={this.handleValueChange}
+            className='br fs-r pr-m pl-m pt-s pb-s bs-bb full'
+          />
+        <button className='bg-dark fc-light'>
+          <SVG fill='#fff' width={20} viewBox='0 0 179.76 179.92' name='search' />
+        </button>
+      </form>
+      {
+        hasValidSearchResults ? (
+          <div className='p-a full br'>
+            <ul className='search-results full bs-bb white-container'>
+            {
+              this.state.searchValue.split('').map(result => (
+                <li className='pl-s pr-s fs-r fw-r pt-s pb-s pl-m'>
+                  <Link to='/hello' className=''>{result}</Link>
+                </li>
+              ))
+            }
+            </ul>
+          </div>
+        ) : null
+      }
+    </div>
     );
   }
 }
